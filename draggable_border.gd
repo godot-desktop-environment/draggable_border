8cfb3afc1c732aa84340ba78215a5679670a221f
thiagola92 <thiagola92@gmail.com>
2026-05-01 13:47:54 -0300

    Use the start_resize() method to resize

--- draggable_border.gd
+++ draggable_border.gd
@@ -2,19 +2,6 @@
 extends VBoxContainer
 
 
-## The term "border" is used to represent each location which the user can drag.
-enum Border {
-	NONE,
-	BOTTOM,
-	BOTTOM_LEFT,
-	BOTTOM_RIGHT,
-	LEFT,
-	RIGHT,
-	TOP,
-	TOP_LEFT,
-	TOP_RIGHT,
-}
-
 ## The term "margin" is used to represent the area which will accept mouse input.
 enum Margin {
 	BOTTOM,
@@ -61,24 +48,7 @@ enum Margin {
 		margin_top = m
 		_resize_margin.call_deferred(Margin.TOP, m)
 
-# Used to know where the edge (bottom right) was when the dragging started.
-# When the user drag the origin (top/left), we move the window and
-# resize so it doesn't pass the edge initial position.
-var _dragging_edge_start: Vector2i
-
-# Used to know where the origin (top left) was when the dragging started.
-# When the user drag the edge (bottom/right), we resize the window but
-# we cannot let them move pass the edge.
-var _dragging_origin_limit: Vector2i
-
-var _is_dragging: bool = false
-
-var _last_hovered: Border = Border.NONE
-
-
-func _process(_delta: float) -> void:
-	if _is_dragging:
-		_on_dragged()
+var _last_hovered: DisplayServer.WindowResizeEdge
 
 
 func _resize_margin(margin: Margin, value: int) -> void:
@@ -106,120 +76,48 @@ func _on_border_input(event: InputEvent) -> void:
 		_on_mouse_button(event)
 
 
-func _on_dragged() -> void:
-	if get_window().mode != Window.MODE_WINDOWED:
-		return
-	
-	var mouse_position: Vector2i = get_global_mouse_position() as Vector2i
-	
-	match _last_hovered:
-		Border.TOP:
-			get_window().position.y = min(
-				get_window().position.y + mouse_position.y,
-				_dragging_origin_limit.y
-			)
-			
-			get_window().size.y = _dragging_edge_start.y - get_window().position.y
-		Border.RIGHT:
-			get_window().size.x = mouse_position.x
-		Border.BOTTOM:
-			get_window().size.y = mouse_position.y
-		Border.LEFT:
-			get_window().position.x = min(
-				get_window().position.x + mouse_position.x,
-				_dragging_origin_limit.x
-			)
-			
-			get_window().size.x = _dragging_edge_start.x - get_window().position.x
-		Border.TOP_RIGHT:
-			get_window().position.y = min(
-				get_window().position.y + mouse_position.y,
-				_dragging_origin_limit.y
-			) # Top
-			
-			get_window().size = Vector2i(
-				mouse_position.x, # Right
-				_dragging_edge_start.y - get_window().position.y, # Top
-			)
-		Border.TOP_LEFT:
-			get_window().position = Vector2i(
-				min(
-					get_window().position.x + mouse_position.x,
-					_dragging_origin_limit.x
-				), # Left,
-				min(
-					get_window().position.y + mouse_position.y,
-					_dragging_origin_limit.y
-				), # Top
-			)
-
-			get_window().size = Vector2i(
-				_dragging_edge_start.x - get_window().position.x, # Left
-				_dragging_edge_start.y - get_window().position.y, # Top
-			)
-		Border.BOTTOM_RIGHT:
-			get_window().size = Vector2i(
-				mouse_position.x, # Right
-				mouse_position.y, # Bottom
-			)
-		Border.BOTTOM_LEFT:
-			get_window().position.x = min(
-				get_window().position.x + mouse_position.x,
-				_dragging_origin_limit.x
-			) # Left
-			
-			get_window().size = Vector2i(
-				_dragging_edge_start.x - get_window().position.x, # Left
-				mouse_position.y, # Bottom
-			)
-
-
 func _on_mouse_button(event: InputEventMouseButton) -> void:
 	if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		_is_dragging = true
-		_dragging_edge_start = get_window().position + get_window().size
-		_dragging_origin_limit = _dragging_edge_start - get_window().min_size
-	elif event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
-		_is_dragging = false
+		get_window().start_resize(_last_hovered)
 
 
 #region Borders signals
 func _on_bottom_gui_input(event: InputEvent) -> void:
-	_last_hovered = Border.BOTTOM
+	_last_hovered = DisplayServer.WINDOW_EDGE_BOTTOM
 	_on_border_input(event)
 
 
 func _on_bottom_left_gui_input(event: InputEvent) -> void:
-	_last_hovered = Border.BOTTOM_LEFT
+	_last_hovered = DisplayServer.WINDOW_EDGE_BOTTOM_LEFT
 	_on_border_input(event)
 
 
 func _on_bottom_right_gui_input(event: InputEvent) -> void:
-	_last_hovered = Border.BOTTOM_RIGHT
+	_last_hovered = DisplayServer.WINDOW_EDGE_BOTTOM_RIGHT
 	_on_border_input(event)
 
 
 func _on_left_gui_input(event: InputEvent) -> void:
-	_last_hovered = Border.LEFT
+	_last_hovered = DisplayServer.WINDOW_EDGE_LEFT
 	_on_border_input(event)
 
 
 func _on_right_gui_input(event: InputEvent) -> void:
-	_last_hovered = Border.RIGHT
+	_last_hovered = DisplayServer.WINDOW_EDGE_RIGHT
 	_on_border_input(event)
 
 
 func _on_top_gui_input(event: InputEvent) -> void:
-	_last_hovered = Border.TOP
+	_last_hovered = DisplayServer.WINDOW_EDGE_TOP
 	_on_border_input(event)
 
 
 func _on_top_left_gui_input(event: InputEvent) -> void:
-	_last_hovered = Border.TOP_LEFT
+	_last_hovered = DisplayServer.WINDOW_EDGE_TOP_LEFT
 	_on_border_input(event)
 
 
 func _on_top_right_gui_input(event: InputEvent) -> void:
-	_last_hovered = Border.TOP_RIGHT
+	_last_hovered = DisplayServer.WINDOW_EDGE_TOP_RIGHT
 	_on_border_input(event)
 #endregion
